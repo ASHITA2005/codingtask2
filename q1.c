dde4f8e6a9ab2be7107f123ee751e061d0ec41f4
Ashita02singh <117788794+Ashita02singh@users.noreply.github.com>
2024-01-20 23:34:33 +0530

    first commit

---
+++ q1.c
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int fib(int n){
+    if (n==0){
+        return 0;
+    }
+    else{
+        return (fib(n-1)+n);
+    }
+    
+}
+int main() {
+    // Write C++ code here
+    int x,n;
+    std::cout << "enter n ";
+    std::cin>>n;
+    x=fib(n);
+    std::cout << "the sum is "<< x;
+
+    return 0;
+}
